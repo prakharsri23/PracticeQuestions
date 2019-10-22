4d4244e6c9e74742e370ef860b90c9e124d7bbf1
Armaanbahl26 <armaanbahl26@gmail.com>
2019-10-22 14:30:36 +0530

    Armaan

--- binary.cpp
+++ binary.cpp
@@ -1,20 +1,19 @@
-#include <stdio.h>
-int main ()
+#include <stdio.h>//including standard input output
+int main ()//in main function
 { 
-  int number=1;
-  int binary=0; 
+  int number=1;//initializing number to 1
+  int binary=0; initializing binary variable to 0
   int n, place=1;
   int remainder;
   printf("Enter number: ");
   scanf("%d", &number );
   n=number;
-  while(n>0)
+  while(n>0)//entering into loop
   { 
     remainder= n%2;
     binary+= remainder*place;
     place*=10;
     n=n/2;
   }
-  printf("Binary is %d", binary);
-  return 0;
+  printf("Binary is %d", binary);//displaying output to console
 }
